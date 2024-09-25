9f2d4eaefea6da06cd2d5e1e52d0f4190dec7e0b
Paulo <paulopocas20@gmail.com>
2024-09-25 19:25:50 -0300

    adicionando icons no footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
+import { IoLogoInstagram } from "react-icons/io";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+
 const Footer=()=>{
     return(
         <>
-       
+        <IoLogoInstagram />
+        <FaFacebookSquare/>
+        <FaLinkedin />
+        <HiOutlineMail />
         </>
     
     )
