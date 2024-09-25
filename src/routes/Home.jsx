c9dd99b1bd7d1f8f80fa457641799e016a242d02
Paulo <paulopocas20@gmail.com>
2024-09-25 19:09:39 -0300

    arrumando o Home e adicionando mais um arquivo de dados.json

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,22 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+
 
 // Importando as imagens
-import sport1 from "/images/sport1.jpg"; 
-import sport2 from "/images/sport2.jpg"; 
-import produto1 from "/images/produto1.jpg"; 
-import produto2 from "/images/produto2.jpg"; 
+import sport1 from '../assets/sport1.jpeg'; 
+import sport2 from '../assets/sport2.jpeg'; 
+import produto1 from '../assets/produto1.jpeg'; 
+import produto2 from '../assets/produto2.png'; 
 
-const Home=()=>{
-    return(
-        <>
-         <section className="home">
+const Home = () => {
+    // Estado para controlar o índice da imagem do slideshow
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const images = [sport1, sport2]; // Array com as imagens
+
+    // Efeito para alternar as imagens do slideshow a cada 3 segundos
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 3000); // Troca a imagem a cada 3 segundos
+
+        return () => clearInterval(interval); // Limpa o intervalo ao desmontar o componente
+    }, [images.length]);
+
+    return (
+        <section className="home">
             <h1>Bem-vindo à Nossa Loja de Esportes</h1>
+            <p>
+                Transforme a sua experiência de condução com
+                a sustentabilidade e inovação! Nossa loja é dedicada
+                a oferecer uma ampla gama de veículos elétricos de alta performance,
+                que combinam eficiência, conforto e respeito ao meio ambiente.
+            </p>
+
             {/* Slideshow de Imagens */}
             <div className="slideshow">
-                <img src={sport1} alt="Esporte 1" />
-                <img src={sport2} alt="Esporte 2" />
-               
+                <img src={images[currentImageIndex]} alt={`Esporte ${currentImageIndex + 1}`} />
             </div>
 
             {/* Cards com produtos */}
@@ -29,10 +46,9 @@ const Home=()=>{
                     <img src={produto2} alt="Produto 2" />
                     <p>Produto 2 - R$150,00</p>
                 </div>
-                
             </div>
         </section>
-        </>
-    )
-}
-export default Home
+    );
+};
+
+export default Home;
