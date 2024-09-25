5c638d92125ba7fe0b2d66036afc9452189e4286
Paulo <paulopocas20@gmail.com>
2024-09-25 18:42:16 -0300

    adicionando login e logout

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,39 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
 
-const Nav=()=>{
-    return(
-        <>
-        <Link to="/">Home</Link>
-        <Link to="/produtos">Produtos</Link>
-        <Link to="/sobre">Sobre</Link>
-        <Link to="/login">Login</Link>
-        </>
-    )
-}
-export default Nav
+const Nav = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        sessionStorage.removeItem("usuario");
+        sessionStorage.removeItem("senha");
+        navigate("/login"); // Redireciona para a página de login
+    };
+
+    const isLoggedIn = sessionStorage.getItem("usuario") !== null;
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/produtos">Produtos</Link>
+                </li>
+                <li>
+                    <Link to="/sobre">Sobre</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+                {isLoggedIn && (
+                    <li>
+                        <button onClick={handleLogout}>Logout</button>
+                    </li>
+                )}
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
