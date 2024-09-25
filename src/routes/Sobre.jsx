4870e4cd9e0cbc2657f5e579aabd8675868e9bd6
Paulo <paulopocas20@gmail.com>
2024-09-25 19:35:28 -0300

    adicionando imagem e texto no sobre e instalando o react icons

--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -1,6 +1,14 @@
+import dev1 from '../assets/dev1.jpg'
+
 const Sobre=()=>{
     return(
         <>
+        <img src={dev1} alt="dev1"/>
+        <p>Paulo Poças - RM556080
+Guilherme Gomes - RM554606
+André Luiz Fernandes De Queiroz - Rm554503
+Israel araujo henriques de Moura
+Rm559068</p>
         </>
     )
 }
